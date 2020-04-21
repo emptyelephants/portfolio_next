3420a2f25b0c31077d17ea0ae062e317b4b6be1f
emptyelephants <emptyelephants@gmail.com>
2020-04-20 22:32:31 -0700

    ran npm audit fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,7 +25,6 @@ module.exports = {
     "react/jsx-filename-extension" : 0,
     "react/react-in-jsx-scope" : 0,
     "react/prop-types" : 0,
+    "import/prefer-default-export": 0,
   },
-    
-
 };
